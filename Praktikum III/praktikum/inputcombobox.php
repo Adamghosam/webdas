1a91a60222f6be28fb8e7da2427d9914722c0117
Adam Ghosam <adamghosam@gmail.com>
2021-07-05 19:17:18 +0700

    menambahkan combobox

---
+++ Praktikum III/praktikum/inputcombobox.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FIlm Kartun Favorit</title>
+</head>
+
+<body>
+    <form action="prosecombobox.php" method="post" name="input">
+        <h2>Pilih Film kartun favorit anda</h2>
+        <select name="Kartun" id="">
+            <option value="Spong Box"> Spong bob</option>
+            <option value="Sincan"> Sincan</option>
+            <option value="Coran"> Conan</option>
+            <option value="Luthfi"> Lutfi</option>
+            <option value="Narto"> naruto</option>
+            <option value="Dragon Boll"> dragon bool</option>
+            <option value="Dora & emon"> doraemon</option>
+        </select>
+        <input type="submit" value="Pilih" name="Pilih">
+    </form>
+
+</body>
+
+</html>
---
+++ Praktikum III/praktikum/prosecombobox.php
@@ -0,0 +1,5 @@
+<?php
+if (isset($_POST['Pilih'])) {
+    $filem = $_POST['Kartun'];
+    echo "Film Kartun Faforit Anda Adalah: <b>$filem</b>";
+}
