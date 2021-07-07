1e9c863207e7fd2414d11ae0b370c613b663db95
Adam Ghosam <adamghosam@gmail.com>
2021-07-07 17:18:43 +0700

    praktikum 11 codenighter

---
+++ praktikum XI/pwlmvc/mvcapp/application/views/home/index.php
@@ -0,0 +1,23 @@
+<!-- slide card menu -->
+
+
+<div class="container">
+    <h1>Menu</h1>
+    <div class="row g-2">
+        <div class="col-3">
+            <div class="p-3 rounded-3 bg-light">
+                <h1 class="align-middle text-center"><i class="fa fa-cloud " style="color:#1259DE"></i></h1>
+                <h4 class="text-center fw-bold">Cloud</h4>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="p-3 rounded-3 bg-light">
+                <h1 class="align-middle text-center"><i class="fa fa-coffee " style="color:#1259DE"></i></h1>
+                <h4 class="text-center fw-bold">Coffee</h4>
+            </div>
+        </div>
+
+
+
+    </div>
+</div>
---
+++ praktikum XI/pwlmvc/mvcapp/application/views/templates/header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- link font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title><?php echo $judul; ?></title>
+</head>
+
+<body style="background-color: #EEEDF5;">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#">POWER OF SALE</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Pricing</a>
+
+                </div>
+            </div>
+        </div>
+    </nav>
